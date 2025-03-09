04828de43cb386457f03822f8f19ad0a1f1a05da
Aisha <111194434+Aisha-Saad@users.noreply.github.com>
2025-03-10 00:28:37 +0300

    Ading 2 sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import Workflow from './components/Workflow'
+import Pricing from './components/Pricing'
 
  const App = () => {
   return (
@@ -8,6 +11,9 @@ import HeroSection from './components/HeroSection'
      <Navbar/>
      <div className='max-w-7xl mx-auto pt-20 px-6'>
      <HeroSection/>
+     <FeatureSection/>
+     <Workflow/>
+     <Pricing/>
      </div>
      
     </>
